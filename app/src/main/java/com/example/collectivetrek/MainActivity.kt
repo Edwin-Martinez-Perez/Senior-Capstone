2824cf57795adf431ed95fe88e10f0a0cc1aed31
Edwin-Martinez-Perez <edwin.martinex02@gmail.com>
2024-04-28 16:50:30 -0500

    4/28/24

--- app/src/main/java/com/example/collectivetrek/MainActivity.kt
+++ app/src/main/java/com/example/collectivetrek/MainActivity.kt
@@ -8,12 +8,7 @@ import androidx.navigation.findNavController
 import com.example.collectivetrek.databinding.ActivityMainBinding
 import com.example.collectivetrek.fragments.GalleryFragment
 import com.example.collectivetrek.fragments.ItineraryFragment
-import com.example.collectivetrek.fragments.LoginFragment
 import com.example.collectivetrek.fragments.ProfileFragment
-import com.example.collectivetrek.fragments.RegisterFragment
-import com.example.collectivetrek.fragments.ResetPasswordFragment
-import com.example.collectivetrek.fragments.WelcomeFragment
-import com.example.collectivetrek.fragments.GroupFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
@@ -24,14 +19,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        //val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         val navController = findNavController(R.id.nav_host_fragment)
         findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        //creates fragment manager
+        val fragmentManager = supportFragmentManager
+        val transaction = fragmentManager.beginTransaction()
         navController.addOnDestinationChangedListener { _, destination, _ ->
             when(destination.id) {
-                R.id.welcomeFragment -> hideBottomNav()
+                R.id.welcomeFragment ->hideBottomNav()
                 R.id.loginFragment -> hideBottomNav()
                 R.id.resetPasswordFragment -> hideBottomNav()
+                R.id.group -> hideBottomNav()
                 else -> showBottomNav ()
             }
         }
@@ -39,52 +37,59 @@ class MainActivity : AppCompatActivity() {
             when (item.itemId) {
                 R.id.profile_item -> {
                     replaceFragment(ProfileFragment())
-                    updateBottomNavigationVisibility()
+                    //updateBottomNavigationVisibility()
                 }
                 R.id.gallery_item -> {
                     replaceFragment(GalleryFragment())
-                    updateBottomNavigationVisibility()
-                }
-                R.id.map_item -> {
-                    replaceFragment(GalleryFragment())
-                    updateBottomNavigationVisibility()
+                    //updateBottomNavigationVisibility()
                 }
                 R.id.itinerary_item -> {
                     replaceFragment(ItineraryFragment())
-                    updateBottomNavigationVisibility()
+                    //updateBottomNavigationVisibility()
                 }
-
                 else -> {
-
                 }
             }
             true
         }
     }
-
     private fun hideBottomNav() {
         binding.bottomNavigation.visibility = View.GONE
     }
-
     private fun showBottomNav() {
         binding.bottomNavigation.visibility = View.VISIBLE
     }
-
     private fun replaceFragment(fragment: Fragment) {
         val fragmentManager = supportFragmentManager //creates fragment manager
         val transaction = fragmentManager.beginTransaction()
         transaction.replace(R.id.nav_host_fragment, fragment)
         transaction.commit()
     }
-
-    private fun updateBottomNavigationVisibility() {
-        val currentFragment = supportFragmentManager.findFragmentById(R.id.frame_layout)
-
-        if (currentFragment is WelcomeFragment || currentFragment is LoginFragment || currentFragment is RegisterFragment ||
-            currentFragment is ResetPasswordFragment) {
-            binding.bottomNavigation.visibility = View.GONE
-        } else {
-            binding.bottomNavigation.visibility = View.VISIBLE
+    /*
+    private fun getActiveFragment(): Fragment {
+        val fragmentManager = supportFragmentManager
+        val fragments = fragmentManager.fragments
+        for (fragment in fragments) {
+            if (fragment != null && fragment.isVisible) {
+                val oldFragment = fragment
+                return fragment
+            }
         }
+        return null
     }
+
+
+
+        private fun updateBottomNavigationVisibility() {
+            val currentFragment = supportFragmentManager.findFragmentById(R.id.frame_layout)
+
+            if (currentFragment is WelcomeFragment || currentFragment is LoginFragment || currentFragment is RegisterFragment ||
+                currentFragment is ResetPasswordFragment) {
+                binding.bottomNavigation.visibility = View.GONE
+            } else {
+                binding.bottomNavigation.visibility = View.VISIBLE
+            }
+        }
+
+     */
 }
